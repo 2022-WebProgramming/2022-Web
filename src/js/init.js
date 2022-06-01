06abc1966b77ffd71d56eadfd6a29fbc70c6edeb
maatanyy <maatanyy@naver.com>
2022-06-02 01:26:54 +0900

    syntax error 수정

---
+++ src/js/init.js
@@ -0,0 +1,301 @@
+const pork = 
+{
+   "name": "돼지고기",
+   "id": "pork",
+   "src": "./img/food/pork.jpg",
+   "alt": "돼지고기",
+   "selected": "false"
+}
+
+const chicken = 
+{
+   "name":"닭고기",
+   "id":"chicken",
+   "src":"./img/food/chicken.jfif",
+   "alt":"닭고기",
+   "selected":"false"
+}
+
+const beef = {
+   "name":"소고기",
+   "id":"beef",
+   "src":"./img/food/beef.jfif",
+   "alt":"소고기",
+   "selected":"false"
+}
+
+const greenOnion = {
+   "name":"대파",
+   "id":"greenOnion",
+   "src":"./img/food/greenOnion.jfif",
+   "alt":"대파",
+   "selected":"false"
+}
+
+const onion = {
+   "name":"양파",
+   "id":"onion",
+   "src":"./img/food/onion.jfif",
+   "alt":"양파",
+   "selected":"false"
+}
+
+const garlic = {
+   "name":"마늘",
+   "id":"garlic",
+   "src":"./img/food/garlic.jpg",
+   "alt":"마늘",
+   "selected":"false"
+}
+
+const carrot = {
+   "name":"당근",
+   "id":"carrot",
+   "src":"./img/food/carrot.jpg",
+   "alt":"당근",
+   "selected":"false"
+}
+
+const potato = {
+   "name":"감자",
+   "id":"potato",
+   "src":"./img/food/potato.jpg",
+   "alt":"감자",
+   "selected":"false"
+}
+
+const cabbage = {
+   "name":"배추",
+   "id":"cabbage",
+   "src":"./img/food/cabbage.jpg",
+   "alt":"배추",
+   "selected":"false"
+}
+
+const lettuce = {
+   "name":"상추",
+   "id":"lettuce",
+   "src":"./img/food/lettuce.jpg",
+   "alt":"상추",
+   "selected":"false"
+}
+
+const cabbage2 = {
+   "name":"양배추",
+   "id":"cabbage2",
+   "src":"./img/food/cabbage2.jpg",
+   "alt":"양배추",
+   "selected":"false"
+}
+
+const mushroom = {
+   "name":"버섯",
+   "id":"mushroom",
+   "src":"./img/food/mushroom.jpg",
+   "alt":"버섯",
+   "selected":"false"
+}
+
+const egg = {
+   "name":"달걀",
+   "id":"egg",
+   "src":"./img/food/egg.jpg",
+   "alt":"달걀",
+   "selected":"false"
+}
+
+const cheese = {
+   "name":"치즈",
+   "id":"cheese",
+   "src":"./img/food/cheese.jpg",
+   "alt":"치즈",
+   "selected":"false"
+}
+
+const tofu = {
+   "name":"두부",
+   "id":"tofu",
+   "src":"./img/food/tofu.jpg",
+   "alt":"두부",
+   "selected":"false"
+}
+
+const shrimp = {
+   "name":"새우",
+   "id":"shrimp",
+   "src":"./img/food/shrimp.jpg",
+   "alt":"새우",
+   "selected":"false"
+}
+
+
+
+   //json은 객체를 문자열로 표현한 방식
+   localStorage.setItem('pork',JSON.stringify(pork));
+   localStorage.setItem('chicken',JSON.stringify(chicken));
+   localStorage.setItem('beef',JSON.stringify(beef));
+   localStorage.setItem('greenOnion',JSON.stringify(greenOnion));
+   localStorage.setItem('onion',JSON.stringify(onion));
+   localStorage.setItem('garlic',JSON.stringify(garlic));
+   localStorage.setItem('carrot',JSON.stringify(carrot));
+   localStorage.setItem('potato',JSON.stringify(potato));
+   localStorage.setItem('cabbage',JSON.stringify(cabbage));
+   localStorage.setItem('lettuce',JSON.stringify(lettuce));
+   localStorage.setItem('cabbage2',JSON.stringify(cabbage2));
+   localStorage.setItem('mushroom',JSON.stringify(mushroom));
+   localStorage.setItem('egg',JSON.stringify(egg));
+   localStorage.setItem('cheese',JSON.stringify(cheese));
+   localStorage.setItem('tofu',JSON.stringify(tofu));
+   localStorage.setItem('shrimp',JSON.stringify(shrimp));
+
+   const salt =
+      {
+         "name": "소금",
+         "id": "salt",
+         "src": "./img/condiment/salt.jpg",
+         "alt": "소금",
+         "selected": "false"
+      }
+
+      const sugar =
+      {
+         "name": "설탕",
+         "id": "sugar",
+         "src": "./img/condiment/sugar.jpg",
+         "alt": "설탕",
+         "selected": "false"
+      }
+
+      const gochutgaryu = {
+         "name": "고춧가루",
+         "id": "gochutgaryu",
+         "src": "./img/condiment/gochutgaryu.jpg",
+         "alt": "고춧가루",
+         "selected": "false"
+      }
+
+      const ganjang = {
+         "name": "간장",
+         "id": "ganjang",
+         "src": "./img/condiment/ganjang.jpg",
+         "alt": "간장",
+         "selected": "false"
+      }
+
+      const doenjang = {
+         "name": "된장",
+         "id": "doenjang",
+         "src": "./img/condiment/doenjang.jpg",
+         "alt": "된장",
+         "selected": "false"
+      }
+
+      const gochujang = {
+         "name": "고추장",
+         "id": "gochujang",
+         "src": "./img/condiment/gochujang.jpg",
+         "alt": "고추장",
+         "selected": "false"
+      }
+
+      const vinegar = {
+         "name": "식초",
+         "id": "vinegar",
+         "src": "./img/condiment/vinegar.jpg",
+         "alt": "식초",
+         "selected": "false"
+      }
+
+      const matsool = {
+         "name": "맛술",
+         "id": "matsool",
+         "src": "./img/condiment/matsool.jpg",
+         "alt": "맛술",
+         "selected": "false"
+      }
+
+      const chamgirum = {
+         "name": "참기름",
+         "id": "chamgirum",
+         "src": "./img/condiment/chamgirum.png",
+         "alt": "참기름",
+         "selected": "false"
+      }
+
+      const gulsauce = {
+         "name": "굴소스",
+         "id": "gulsauce",
+         "src": "./img/condiment/gulsauce.jpg",
+         "alt": "굴소스",
+         "selected": "false"
+      }
+
+      const cookingoil = {
+         "name": "식용유",
+         "id": "cookingoil",
+         "src": "./img/condiment/cookingoil.jpg",
+         "alt": "식용유",
+         "selected": "false"
+      }
+
+      const ketchup = {
+         "name": "케첩",
+         "id": "ketchup",
+         "src": "./img/condiment/ketchup.jpg",
+         "alt": "케첩",
+         "selected": "false"
+      }
+
+      const mayonnaise = {
+         "name": "마요네즈",
+         "id": "mayonnaise",
+         "src": "./img/condiment/mayonnaise.jpg",
+         "alt": "마요네즈",
+         "selected": "false"
+      }
+
+      const starchsyrup = {
+         "name": "물엿",
+         "id": "starchsyrup",
+         "src": "./img/condiment/starchsyrup.jpg",
+         "alt": "물엿",
+         "selected": "false"
+      }
+
+      const pepper = {
+         "name": "후추",
+         "id": "pepper",
+         "src": "./img/condiment/pepper.png",
+         "alt": "후추",
+         "selected": "false"
+      }
+
+      const anchovy = {
+         "name": "멸치액젓",
+         "id": "anchovy",
+         "src": "./img/condiment/anchovysauce.jpg",
+         "alt": "멸치액젓",
+         "selected": "false"
+      }
+
+    
+
+      localStorage.setItem('salt', JSON.stringify(salt));
+      localStorage.setItem('sugar', JSON.stringify(sugar));
+      localStorage.setItem('gochutgaryu', JSON.stringify(gochutgaryu));
+      localStorage.setItem('ganjang', JSON.stringify(ganjang));
+      localStorage.setItem('doenjang', JSON.stringify(doenjang));
+      localStorage.setItem('gochujang', JSON.stringify(gochujang));
+      localStorage.setItem('vinegar', JSON.stringify(vinegar));
+      localStorage.setItem('matsool', JSON.stringify(matsool));
+      localStorage.setItem('chamgirum', JSON.stringify(chamgirum));
+      localStorage.setItem('gulsauce', JSON.stringify(gulsauce));
+      localStorage.setItem('cookingoil', JSON.stringify(cookingoil));
+      localStorage.setItem('ketchup', JSON.stringify(ketchup));
+      localStorage.setItem('mayonnaise', JSON.stringify(mayonnaise));
+      localStorage.setItem('starchsyrup', JSON.stringify(starchsyrup));
+      localStorage.setItem('pepper', JSON.stringify(pepper));
+      localStorage.setItem('anchovy', JSON.stringify(anchovy));
+
+
+      
